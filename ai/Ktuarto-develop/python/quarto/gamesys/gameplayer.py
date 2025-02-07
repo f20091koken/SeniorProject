b7cff3351f423772808e84cbdf7db40c606ff91b
kairi_1528 <73269230+gaku247824@users.noreply.github.com>
2025-02-07 13:03:50 +0900

    aiと画像認識を繋げる部分
    俺はimage_recognition_result()関数を作成する

--- ai/Ktuarto-develop/python/quarto/gamesys/gameplayer.py
+++ ai/Ktuarto-develop/python/quarto/gamesys/gameplayer.py
@@ -72,8 +72,23 @@ class ManualAi(base_ai.BaseAi):
 
         #手入力処理
         while(res_left is None):
-            
+            """"
+            image_recognition_result=image_recognition_result()
+            print(image_recognition_result)
+            print('コマの配置場所があっていたら「cキー」、間違っていたら「rキー」を入力してください')
+            while True:
+                key = input()  # キー入力を待機
+                if key == 'c':  # cが入力されたら続行
+                    break
+                elif key == 'r':  # rが入力されたら修正処理
+                    image_recognition_result = input('修正する駒の配置を入力してください')
+            """
+
+
             in_str = [int(i) for i in input('put(left top) >>').split()]
+            """"
+            in_str = [int(i) for i in image_recognition_result.split()]
+            """
 
             #範囲外の数字を入力
             if not (0 <= in_str[0] and in_str[0] <= 3 and 0 <= in_str[1] and in_str[1] <= 3):continue
